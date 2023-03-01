735098ce9e060396afbcdaf2ac0a23ba8a83c9f9
ribeiroAllison <allisonribeiro@gmail.com>
2023-03-01 11:30:14 -0300

    add buttons to article section of OverciewMercury component, several CSS changes

--- src/Components/Mercury/OverviewMercury.js
+++ src/Components/Mercury/OverviewMercury.js
@@ -1,4 +1,5 @@
 import '../../Components/Global.css';
+import { NavLink } from 'react-router-dom';
 
 export default function OverviewMercury (){
 
@@ -14,10 +15,25 @@ export default function OverviewMercury (){
                 Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.
                 </p>
                 <div className='source'>
-                    <p id="source-label">Source:</p><a href="https://en.wikipedia.org/wiki/Mercury_(planet)" target='_blank'>Wikipedia</a>
+                    <p id="source-label">Source:</p>
+                    <a href="https://en.wikipedia.org/wiki/Mercury_(planet)" target='_blank'>Wikipedia</a>
                 </div>
-                
+                <div id='buttons'>
+                    <NavLink to="/mercury">
+                        <p className='buttonNumber'>01</p>
+                        <p>OVERVIEW</p>
+                    </NavLink>
+                    <NavLink to="/">
+                        <p className='buttonNumber'>02</p>
+                        <p>INTERNAL STRUCTURE</p>
+                    </NavLink>
+                    <NavLink to="/">
+                        <p className='buttonNumber'>02</p>
+                        <p>SURFACE GEOLOGY</p>
+                    </NavLink>
+                </div>
             </article>
+            
         </div>
     )
 }
